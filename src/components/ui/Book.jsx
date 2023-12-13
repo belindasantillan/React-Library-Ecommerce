131f81bba3b05e59908f3e69610ed6e37292f2e1
Belinda Santillan <belindasarahisantillan@gmail.com>
2023-12-13 13:06:13 -0600

    Format Document

--- src/components/ui/Book.jsx
+++ src/components/ui/Book.jsx
@@ -4,45 +4,48 @@ import Price from "./Price";
 import Rating from "./Rating";
 
 const Book = ({ book }) => {
-    const [img, setImg] = useState();
+  const [img, setImg] = useState();
 
-    useEffect(() => {
-        const image = new Image();
-        image.src = book.url;
-        image.onload = () => {
-            setTimeout(() => {
-              setImg(image);
-            }, 400);
-        };
-    });
+  useEffect(() => {
+    const image = new Image();
+    image.src = book.url;
+    image.onload = () => {
+      setTimeout(() => {
+        setImg(image);
+      }, 400);
+    };
+  });
 
-    return(
-        <div className="book">
-            {img ? (
-                <>
-                    <Link to={`/books/${book.id}`}>
-                        <figure className="book__img--wrapper">
-                                <img src={img.src} alt="" className="book__img" />
-                        </figure>
-                    </Link>
-                    <div className="book__title">
-                        <Link to={`/books/${book.id}`} className="book__title--link">
-                            {book.title}
-                        </Link>
-                    </div>
-                    <Rating rating={book.rating}/>
-                    <Price salePrice={book.salePrice} originalPrice={book.originalPrice} />
-                </>
-            ) : (
-                <>
-                    <div className="book__img--skeleton"></div>
-                    <div className="skeleton book__title--skeleton"></div>
-                    <div className="skeleton book__rating--skeleton"></div>
-                    <div className="skeleton book__price--skeleton"></div>
-                </>
-            )}
-        </div>
-    );
+  return (
+    <div className="book">
+      {img ? (
+        <>
+          <Link to={`/books/${book.id}`}>
+            <figure className="book__img--wrapper">
+              <img src={img.src} alt="" className="book__img" />
+            </figure>
+          </Link>
+          <div className="book__title">
+            <Link to={`/books/${book.id}`} className="book__title--link">
+              {book.title}
+            </Link>
+          </div>
+          <Rating rating={book.rating} />
+          <Price
+            salePrice={book.salePrice}
+            originalPrice={book.originalPrice}
+          />
+        </>
+      ) : (
+        <>
+          <div className="book__img--skeleton"></div>
+          <div className="skeleton book__title--skeleton"></div>
+          <div className="skeleton book__rating--skeleton"></div>
+          <div className="skeleton book__price--skeleton"></div>
+        </>
+      )}
+    </div>
+  );
 };
 
 export default Book;
